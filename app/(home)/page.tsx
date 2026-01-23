1827ea813042e719c5b9900ac7da475cf50e0c51
Kyla Yu <kylayu@gmail.com>
2026-01-23 11:00:57 -0500

    Fixed page routing and relinking home button

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import SubHeader from "@/app/components/text/h2";
-import MaxWidthWrapper from "../../components/max-width-wrapper";
-import Header from "../../components/text/h1";
+import Header from "../components/text/h1";
+
 
 export default function Home() {
   return (
