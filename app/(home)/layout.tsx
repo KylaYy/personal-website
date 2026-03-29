22768340ec0e17be17e7db7747c7d3fcd608552f
Kyla Yu <kylayu@gmail.com>
2026-03-29 14:48:45 -0400

    Updated default font size

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "../components/max-width-wrapper";
 import NavBar from "../components/navbar";
+import TextWrapper from "../components/text-wrapper";
 
 export default function HomeLayout({
   children,
