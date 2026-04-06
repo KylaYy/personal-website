c25f024222f6ffe9896a7599f8e0987b068121ac
Kyla Yu <kylayu@gmail.com>
2026-04-06 11:46:39 -0400

    Linked Linkedin to image

--- app/(home)/contact/page.tsx
+++ app/(home)/contact/page.tsx
@@ -1,10 +1,17 @@
 import TextWrapper from "@/app/components/text-wrapper";
 import Header from "@/app/components/text/h1";
+import Paragraph from "@/app/components/text/paragraph";
+import Image from 'next/image'
+
 
 export default function Contact() {
   return (
     <TextWrapper>
       <Header>Contact</Header>
+      <Paragraph>Reach out!</Paragraph>
+      <a href='https://www.linkedin.com/in/k--ylayu27/' target='_blank'>
+      <Image src='/images/linkedin.JPG' alt='bear with text bubble containing linkedin logo' aria-label="i ate the linkedin profile, click me!" width={300} height={200}></Image>
+      </a>
     </TextWrapper>
   );
 }
