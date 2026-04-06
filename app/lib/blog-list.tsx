9447ca7e851ec49c59bca0068b9a5eac17ae6933
Kyla Yu <kylayu@gmail.com>
2026-04-06 13:21:10 -0400

    Tested markdown filepat

--- app/lib/blog-list.tsx
+++ app/lib/blog-list.tsx
@@ -12,7 +12,7 @@ interface Post {
 // List containing all blog posts and slugs
 const postList: Post[] = [
     {key: 1, title: "blog post 1", slug: "/1", date: "valid/date/day", desc: "This is a test description form the posts list"},
-    {key: 2, title: "blog post 2", slug: "/2", date: "valid/date/day", desc: "This is a test description form the posts list"},
+    {key: 2, title: "Markdown Test", slug: "/mdx-test", date: "valid/date/day", desc: "This is a test description form the posts list"},
     {key: 3, title: "blog post 3", slug: "/3", date: "valid/date/day", desc: "This is a test description form the posts list"}
 ];
 
